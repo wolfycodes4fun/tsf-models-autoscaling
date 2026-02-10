ea331c7904049fd85aefd139aeda7dd8f29c12b8
Ametha Jayawardena 🐺 <amethacodes@gmail.com>
2026-02-10 20:28:58 +0530

    merge branch 'ametha/containerize_autoscaler' to 'main'
    * tons of changes
    
    * fix incorrect file format to store the model
    
    * modify dockerfile to copy only relevant content to container
    
    * another bunch of changes
    
    * minor changes
    
    ---------
    
    Co-authored-by: ajaylk <ametha.jayawardena@ifs.com>

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.11-slim
+
+WORKDIR /app
+
+# Install dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy source code
+COPY src/scalers/ ./src/scalers/
+COPY src/utils/ ./src/utils/
+COPY src/config.py ./src/
+COPY src/holt_winters.py ./src/
+COPY models/ ./models/
+
+EXPOSE 50051
+
+CMD ["python", "-m", "src.scalers.keda_grpc_server"]
---
+++ src/tests/test.py
@@ -0,0 +1,37 @@
+import requests
+import time
+import threading
+
+# Configuration
+# Use port-forward: kubectl port-forward -n python-demo-app svc/demo-flask-app-for-autoscaling-service 8080:80
+URL = "http://20.204.246.223/api/hello" # Replace with your service URL
+TOTAL_THREADS = 50  # Increase this to scale higher
+
+def send_requests():
+    print(f"Thread started...")
+    while True:
+        try:
+            # Mimic 20 requests per second per thread
+            response = requests.get(URL, timeout=1)
+            time.sleep(0.05) 
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    print(f"Starting load test on {URL}...")
+    threads = []
+    
+    # Ramp up: Add 5 threads every 10 seconds
+    for i in range(0, TOTAL_THREADS, 5):
+        for _ in range(5):
+            t = threading.Thread(target=send_requests)
+            t.daemon = True
+            t.start()
+            threads.append(t)
+        
+        print(f"Current Load: {len(threads)} threads active...")
+        time.sleep(10)
+
+    # Keep the script running
+    while True:
+        time.sleep(1)
